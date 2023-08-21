bfac84092af1cf3c67d9286b9ce4c281e0efad7f
Rayed Suhail Ahmad <rayedsuhail@gmail.com>
2023-08-21 12:21:57 -0500

    Update type definitions in image_loader.py

--- script/image_loader.py
+++ script/image_loader.py
@@ -10,13 +10,13 @@ from PIL import Image
 import numpy as np
 
 
-def generate_labels_for_category(label_path: str):
+def generate_labels_for_category(label_path: str) -> list[str]:
     return open(label_path).readlines()
 
 def get_path_and_category(path_category: str) -> (str, str):
     return path_category.split(maxsplit = 1)
 
-def fetch_img(img_path):
+def fetch_img(img_path: str) -> np.ndarray:
     try:
         file = Image.open(f'../dataset/images/{img_path}')
         img = np.asarray(file.resize((100, 150), Image.ANTIALIAS).copy())
@@ -25,12 +25,12 @@ def fetch_img(img_path):
     except Exception as e:
         print(e)
 
-def two_random_imgs(img_type: str, all_imgs):
+def two_random_imgs(img_type: str, all_imgs: dict) -> list[np.ndarray]:
     samples = random.sample(list(filter(lambda img: img['category'] == img_type, all_imgs)), 2)
-    return [fetch_img(img['path']) for img in samples]
+    return list(fetch_img(img['path']) for img in samples)
 
-def get_all_imgs_dictlist(paths: dict) -> (list):
-    all_imgs = []
+def get_all_imgs_dictlist(paths: dict) -> (list[dict]):
+    all_imgs = list()
     for key in paths:
         for path_category in generate_labels_for_category(paths[key]):
             path, category = get_path_and_category(path_category)
