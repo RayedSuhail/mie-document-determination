e52f7cc833b9f2b1e4624e6ca323c346ff71bb70
Rayed Suhail Ahmad <rayedsuhail@gmail.com>
2023-08-23 17:28:31 -0500

    Update return types of functions in image_loader.py

--- script/image_loader.py
+++ script/image_loader.py
@@ -8,9 +8,10 @@ Created on Tue Jul  4 14:33:44 2023
 import random
 from PIL import Image
 import numpy as np
+from typing import List
 
 
-def generate_labels_for_category(label_path: str) -> list[str]:
+def generate_labels_for_category(label_path: str) -> List[str]:
     return open(label_path).readlines()
 
 def get_path_and_category(path_category: str) -> (str, str):
@@ -25,11 +26,11 @@ def fetch_img(img_path: str) -> np.ndarray:
     except Exception as e:
         print(e)
 
-def two_random_imgs(img_type: str, all_imgs: dict) -> list[np.ndarray]:
+def two_random_imgs(img_type: str, all_imgs: dict) -> List[np.ndarray]:
     samples = random.sample(list(filter(lambda img: img['category'] == img_type, all_imgs)), 2)
     return list(fetch_img(img['path']) for img in samples)
 
-def get_all_imgs_dictlist(paths: dict) -> (list[dict]):
+def get_all_imgs_dictlist(paths: dict) -> List[dict]:
     all_imgs = list()
     for key in paths:
         for path_category in generate_labels_for_category(paths[key]):
